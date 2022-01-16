73aea4fd3f5ebf9a3c8516803f2b910bcb881cb2
Polina-Oleshkevich <94075525+Polina-Oleshkevich@users.noreply.github.com>
2022-01-16 19:39:12 +0300

    Add files via upload

---
+++ ITMO.CSCourse.WFApp.Lab1.2/nForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.CSCourse.WFApp.Lab1._2
+{
+    public partial class nForm : ITMO.CSCourse.WFApp.Lab1._2.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+            Application.Run(new nForm());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
