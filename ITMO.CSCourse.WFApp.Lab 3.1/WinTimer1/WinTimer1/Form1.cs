8e91fdcbd91f182f79259264ac677bc7b0ee135c
Polina-Oleshkevich <94075525+Polina-Oleshkevich@users.noreply.github.com>
2022-01-19 17:57:27 +0300

    Add files via upload

---
+++ ITMO.CSCourse.WFApp.Lab 3.1/WinTimer1/WinTimer1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTimer1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userControlTimer1.Enabled = !userControlTimer1.Enabled;
+            if (userControlTimer1.Enabled == true)
+            {
+                TimeEnabled = false; 
+            }
+            else
+            {
+                TimeEnabled = true; 
+            }
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ITMO.CSCourse.WFApp.Lab 3.1/WinTimer1/WinTimer1/UserControlTimer.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace WinTimer1
+{
+    partial class UserControlTimer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.WinTimer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(283, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // WinTimer1
+            // 
+            this.WinTimer1.Enabled = true;
+            this.WinTimer1.Interval = 1000;
+            this.WinTimer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // UserControlTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "UserControlTimer";
+            this.Size = new System.Drawing.Size(433, 243);
+            this.Load += new System.EventHandler(this.UserControlTimer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer WinTimer1;
+    }
+}
