de17d090c41fae76c5608d432796fe6913e7a11c
Polina-Oleshkevich <94075525+Polina-Oleshkevich@users.noreply.github.com>
2022-01-20 09:25:13 +0300

    Add files via upload

---
+++ ITMO.CSCourse.WFApp.Lab 3.1.1/WinTimer1/WinTimer1/UserControlTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTimer1
+{
+    public partial class UserControlTimer : UserControl
+    {
+        public UserControlTimer()
+        {
+            InitializeComponent();
+        }
+        public bool TimeEnabled 
+        { get { return MainTimer.Enabled; } 
+            set { MainTimer.Enabled = value; } 
+        }
+        private void dateTimePicker1_ValueChanget(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+
+        }
+       
+    }
+}
